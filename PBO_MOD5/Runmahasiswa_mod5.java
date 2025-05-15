131bae71fc6b889ad08bda3277cd7499fb80f482
FAHMI AL-ANSHORI <139199297+FAHMIALAN@users.noreply.github.com>
2025-05-16 00:12:55 +0700

    Delete PBO_MOD5 directory

--- PBO_MOD5/Runmahasiswa_mod5.java
+++
@@ -1,8 +0,0 @@
-public class Runmahasiswa_mod5 {
-   public static void main(String args[]) {
-      Mahasiswa_mod5 encap = new Mahasiswa_mod5();
-      encap.setNama("ALAN");
-      encap.setJurusan("INFORMATIKA");
-      encap.setUmur(21);
-      System.out.print("Nama : " + encap.getNama() + " Jurusan : " + encap.getJurusan() + " Umur : " + encap.getUmur());
-   }}
--- PBO_MOD5/paket_mod5.java
+++
@@ -1,8 +0,0 @@
-package mahasiswa;
-
-public class paket_mod5 {
-
-    public static void isiPaket() {
-        System.out.println("ini adalah hasil import");
-    }
-}
