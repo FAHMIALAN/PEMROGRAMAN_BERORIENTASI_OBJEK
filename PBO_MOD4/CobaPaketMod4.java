9eb3671296b6703a286ddbb880668d8e95275b25
FAHMI AL-ANSHORI <139199297+FAHMIALAN@users.noreply.github.com>
2025-05-16 00:35:56 +0700

    Add files via upload

---
+++ PBO_MOD4/CobaPaketMod4.java
@@ -0,0 +1,10 @@
+
+import mahasiswa.*;
+
+public class CobaPaketMod4 {
+
+    public static void main(String args[]) {
+        PaketMod4 coba = new PaketMod4(); // Gunakan nama kelas yang benar
+        PaketMod4.isiPaket(); // Panggil metode dengan menggunakan nama kelas
+    }
+}
